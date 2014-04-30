fd27c80ccdc49d0470936b2ea82e54f98ef91b1d
Nick LaGrow <nick@lagrow.com>
2014-04-30 15:40:09 -0400

    Add packaged app

--- extension/script/xhr_override.js
+++ extension/script/xhr_override.js
@@ -91,18 +91,6 @@
       nState    = this.readyState,
       fOnUnload;
 
-    // BUGFIX: IE - memory leak on page unload (inter-page leak)
-    if (bIE && bAsync) {
-      fOnUnload = function() {
-        if (nState != cXMLHttpRequest.DONE) {
-          fCleanTransport(oRequest);
-          // Safe to abort here since onreadystatechange handler removed
-          oRequest.abort();
-        }
-      };
-      window.attachEvent("onunload", fOnUnload);
-    }
-
     // Add method sniffer
     if (cXMLHttpRequest.onopen)
       cXMLHttpRequest.onopen.apply(this, arguments);
@@ -139,10 +127,6 @@
         // Free up queue
         delete oRequest._data;
         fCleanTransport(oRequest);
-
-        // BUGFIX: IE - memory leak in interrupted
-        if (bIE && bAsync)
-          window.detachEvent("onunload", fOnUnload);
       }
 
       // BUGFIX: Some browsers (Internet Explorer, Gecko) fire OPEN readystate twice
@@ -352,20 +336,7 @@
   };
 
   function fCleanTransport(oRequest) {
-    // BUGFIX: IE - memory leak (on-page leak)
-    // oRequest._object.onreadystatechange = new window.Function;
     oRequest._object.onreadystatechange = null;
-  };
-
-  // Internet Explorer 5.0 (missing apply)
-  if (!window.Function.prototype.apply) {
-    window.Function.prototype.apply  = function(oRequest, oArguments) {
-      if (!oArguments)
-        oArguments  = [];
-      oRequest.__func  = this;
-      oRequest.__func(oArguments[0], oArguments[1], oArguments[2], oArguments[3], oArguments[4]);
-      delete oRequest.__func;
-    };
   };
 
   // enable toggling between XHR versions
